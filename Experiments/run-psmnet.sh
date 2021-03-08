42e38413986f8c4be6d706c08c010fc6422de4c9
Manoj Rajagopalan <rajagopalan.manoj@gmail.com>
2021-03-08 06:22:16 +0000

    Specialize run-psmnet.sh to AWS env

--- Experiments/run-psmnet.sh
+++ Experiments/run-psmnet.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-PY3=${PY3:-/usr/bin/python3}
+PY3=python3
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-BASE_DIR=${BASE_DIR:-${SCRIPT_DIR}/../../KITTI-2015/Raw_Data}
-DATE=${DATE:-2011_09_26}
-DRIVE=${DRIVE:-0001}
-WEIGHTS=${WEIGHTS:-${SCRIPT_DIR}/../../psmnet-pretrained_model_KITTI2015.tar}
+BASE_DIR=/cs231a/data/kitti
+DATE=2011_09_26
+DRIVE=0001
+WEIGHTS=/cs231a/psmnet-pretrained_model_KITTI2015.tar
 
 # echo 'run.sh is using: '
 # echo " - BASE_DIR = ${BASE_DIR}"
@@ -14,7 +14,7 @@ WEIGHTS=${WEIGHTS:-${SCRIPT_DIR}/../../psmnet-pretrained_model_KITTI2015.tar}
 # echo " - WEIGHTS = ${WEIGHTS}"
 # echo
 
-CMD="${PY3} pykitti-psmnet.py --base-dir ${BASE_DIR} \
+CMD="${PY3} pykitti-psmnet.py --base_dir ${BASE_DIR} \
                               --date ${DATE} \
                               --drive ${DRIVE} \
                               --pretrained_weights_path ${WEIGHTS}"
